f44433dbfdf7cf95461ab74426ab2b6d5f3fa1a8
IlyaHar <harcenkoila278gmail.com>
2024-01-09 17:25:34 +0200

    comments better

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+require_once 'helpers.php';
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -24,12 +27,10 @@
 	</form>
 	
 	<?php
-		$page_id = 150; // Уникальный идентификатор страницы (статьи или поста)
-		$mysqli = new mysqli("localhost", "root", "", "db"); // Подключается к базе данных
-		$result_set = $mysqli->query("SELECT * FROM `comments` WHERE `page_id`='$page_id'"); //Вытаскиваем все комментарии для данной страницы
-		while ($row = $result_set->fetch_assoc()) {
-			print_r($row); //Вывод комментариев
-			echo "<br />";		}
+
+    printResult(150);
+
+
 	?>
 </body>
 </html>
